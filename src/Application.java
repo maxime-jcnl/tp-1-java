42ae2112bb919134e57935f9a38173c23a583743
Your Name <maxime.jaconelli@efrei.net>
2024-03-18 18:05:56 +0100

    esdqsqd

--- src/Application.java
+++ src/Application.java
@@ -5,25 +5,22 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Création des clients
-        // Création des clients
         Client[] clients = new Client[3];
         for (int i = 0; i < 3; i++) {
             System.out.println("Entrez les informations pour le client " + (i + 1) + ":");
             System.out.print("ID: ");
             int ID = scanner.nextInt();
-            scanner.nextLine(); // Pour consommer la nouvelle ligne restante après nextInt()
+            scanner.nextLine();
             System.out.print("Nom: ");
             String nom = scanner.nextLine();
             System.out.print("Prénom: ");
             String prenom = scanner.nextLine();
-            // Création d'un compte bancaire avec un solde initial de 0 pour chaque client
             CompteBancaire compte = new CompteBancaire();
             clients[i] = new Client(ID, nom, prenom, compte);
         }
 
         Client deuxiemeClient = clients[1];
-        System.out.println("\nDépôt de 5000 euros pour le deuxième client...");
+        System.out.println("\nDépot de 5000 euros pour le deuxième client...");
         deuxiemeClient.depot(5000);
         deuxiemeClient.compte.changerTauxInteret(0.1); // Changement du taux d'intérêt à 0.1
         System.out.println("Solde actuel du compte du deuxième client après dépôt: ");
@@ -33,6 +30,8 @@ public class Application {
         System.out.println("ID: " + deuxiemeClient.ID);
         System.out.println("Nom: " + deuxiemeClient.nom);
         System.out.println("Prénom: " + deuxiemeClient.prenom);
+        System.out.println("Taux d'intérêt du compte du deuxième client: " + deuxiemeClient.compte.interet);
+
         System.out.println("Solde du compte: " + deuxiemeClient.compte.solde);
 
         System.out.println("\nSolde disponible sur chacun des comptes:");
