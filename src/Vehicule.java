e17145aed96519dbd9f68b0855c4b8b1f1dc9b3c
Your Name <maxime.jaconelli@efrei.net>
2024-03-18 17:04:02 +0100

    esdqsqd

--- src/Vehicule.java
+++ src/Vehicule.java
@@ -4,11 +4,19 @@ public class Vehicule {
     static int Nombre = 0; // Attribut de classe Nombre
 
     Vehicule(){
+        Nombre++; // Incrémentation lors de la création d'un nouvel objet Vehicule
     }
     Vehicule(String marque, int vitesse_max, int vitesse_cour, int PUISSANCE_FISC){
         this.marque = marque;
         this.vitesse_max= vitesse_max;
         this.vitesse_cour = vitesse_cour;
         this.PUISSANCE_FISC = PUISSANCE_FISC;
+        Nombre++; // Incrémentation lors de la création d'un nouvel objet Vehicule
+    }
+    public void supprimerVehicule() {
+        Nombre--; // Décrémentation lors de la suppression d'un objet Vehicule
+    }
+    public static int getNombreVehicules() {
+        return Nombre;
     }
 }
